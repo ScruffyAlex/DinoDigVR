574ca8e74b0a3099837e9546f44172f29bb01bd1
Alex McDowell <scruffyalexhs@gmail.com>
2021-10-22 22:25:33 -0600

    better generation, working towards Digging

--- Assets/Scripts/Voxel/BiomeGenerator.cs
+++ Assets/Scripts/Voxel/BiomeGenerator.cs
@@ -8,41 +8,43 @@ public class BiomeGenerator : MonoBehaviour
     public int waterThreshold = 50;
     public NoiseSettings biomeNoiseSettings;
 
+    public DomainWarping domainWarping;
+
+    public bool useDomainWarping = true;
+
+    public BlockLayerHandler startLayerHandler;
+
+    public List<BlockLayerHandler> additionalLayerHandlers;
+
     internal ChunkData ProccessChunkColumn(ChunkData data, int x, int z, Vector2Int mapSeedOffset)
     {
-        //float noiseValue = Mathf.PerlinNoise((mapSeedOffset.x + data.worldPosition.x + x) * noiseScale, (mapSeedOffset.y + data.worldPosition.z + z) * noiseScale);
-        //int groundPosition = Mathf.RoundToInt(noiseValue * data.chunkHeight);
         biomeNoiseSettings.worldOffset = mapSeedOffset;
         int groundPosition = GetSurfaceHeightNoise(data.worldPosition.x + x, data.worldPosition.z + z, data.chunkHeight);
 
         for (int y = 0; y < data.chunkHeight; y++)
         {
-            BlockType voxelType = BlockType.Dirt;
-            if (y > groundPosition)
-            {
-                if (y < waterThreshold)
-                {
-                    voxelType = BlockType.Water;
-                }
-                else
-                {
-                    voxelType = BlockType.Air;
-                }
-
-            }
-            else if (y == groundPosition)
-            {
-                voxelType = BlockType.Grass_Dirt;
-            }
-
-            Chunk.SetBlock(data, new Vector3Int(x, y, z), voxelType);
+            startLayerHandler.Handle(data, x, y, z, groundPosition, mapSeedOffset);
         }
+
+        foreach (var layer in additionalLayerHandlers)
+        {
+            layer.Handle(data, x, data.worldPosition.y, z, groundPosition, mapSeedOffset);
+        }
+
         return data;
     }
 
     private int GetSurfaceHeightNoise(int x, int z, int chunkHeight)
     {
-        float terrainHeight = MyNoise.OctavePerlin(x, z, biomeNoiseSettings);
+        float terrainHeight;
+        if(useDomainWarping == false)
+        {
+           terrainHeight = MyNoise.OctavePerlin(x, z, biomeNoiseSettings);
+        }
+        else
+        {
+            terrainHeight = domainWarping.GenerateDomainNoise(x, z, biomeNoiseSettings);
+        }
         terrainHeight = MyNoise.Redistribution(terrainHeight, biomeNoiseSettings);
         int surfaceHeight = MyNoise.RemapValue01ToInt(terrainHeight, 0, chunkHeight);
         return surfaceHeight;

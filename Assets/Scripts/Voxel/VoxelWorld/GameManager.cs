c046845b7f64a8851166badb6fff84ac513603e4
Alex McDowell <scruffyalexhs@gmail.com>
2021-11-03 16:33:25 -0600

    working menu system

--- Assets/Scripts/Voxel/VoxelWorld/GameManager.cs
+++ Assets/Scripts/Voxel/VoxelWorld/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,9 +16,34 @@ public class GameManager : MonoBehaviour
     float detectionTime = 1;
     public Camera mainCam;
 
+    public UIManager uiManager;
+
+    public static GameState currentState = GameState.TITLE;
+
+    public GameObject biomeIcon1;
+    public GameObject biomeIcon2;
+    public GameObject mainTitle;
+    public GameObject biomeSelect;
+    public GameObject aboutPage;
+    public GameObject pausePanel;
+
+    public enum GameState
+    {
+       TITLE,
+       BIOMECHOOSE,
+       GAME,
+       MUSEUM,
+       PAUSED,
+       ABOUT,
+       EXITGAME,
+    }
+
     public void Start()
     {
-        world.GenerateWorld();
+        if(currentState == GameState.GAME)
+        {
+            world.GenerateWorld();
+        }
     }
     public void SpawnPlayer()
     {
@@ -51,7 +77,6 @@ public class GameManager : MonoBehaviour
             )
         {
             world.LoadAdditionalChunksRequest(player);
-
         }
         else
         {
@@ -64,5 +89,105 @@ public class GameManager : MonoBehaviour
         currentPlayerChunkPosition = WorldDataHelper.ChunkPositionFromBlockCoords(world, Vector3Int.RoundToInt(player.transform.position));
         currentChunkCenter.x = currentPlayerChunkPosition.x + world.chunkSize / 2;
         currentChunkCenter.z = currentPlayerChunkPosition.z + world.chunkSize / 2;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            OnPause();
+        }
+
+        switch (currentState)
+        {
+            case GameState.TITLE:
+                Time.timeScale = 1;
+                break;
+            case GameState.BIOMECHOOSE:
+                biomeSelect.SetActive(true);
+                mainTitle.SetActive(false);
+                biomeIcon1.SetActive(true);
+                biomeIcon2.SetActive(true);
+                break;
+            case GameState.GAME:
+                Time.timeScale = 1;
+                //Cursor.lockState = CursorLockMode.Locked;
+                break;
+            case GameState.MUSEUM:
+                break;
+            case GameState.PAUSED:
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                Time.timeScale = 0;
+                pausePanel.SetActive(true);
+                break;
+            case GameState.ABOUT:
+                mainTitle.SetActive(false);
+                aboutPage.SetActive(true);
+                break;
+            case GameState.EXITGAME:
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void StartGameDesert()
+    {
+        currentState = GameState.GAME;
+        SceneManager.LoadScene("TerrainGenTest");
+    }
+
+    public void StartGameMountain()
+    {
+        currentState = GameState.GAME;
+        SceneManager.LoadScene("DesertGen");
+    }
+
+    public void SelectBiome()
+    {
+        currentState = GameState.BIOMECHOOSE;
+
+    }
+
+    public void AboutPage()
+    {
+        currentState = GameState.ABOUT;
+    }
+
+    public void BackButton()
+    {
+        currentState = GameState.TITLE;
+    }
+
+    public void OnPause()
+    {
+        currentState = GameState.PAUSED;
+    }
+
+    public void ReturnToTitle()
+    {
+        currentState = GameState.TITLE;
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    public void ReturnToGame()
+    {
+        currentState = GameState.GAME;
+        pausePanel.SetActive(false);
+    }
+
+    public void ToMuseum()
+    {
+        currentState = GameState.MUSEUM;
+        SceneManager.LoadScene("Museum");
+    }
+
+    public void PauseBackButton()
+    {
+        //Cursor.lockState = CursorLockMode.None;
+        currentState = GameState.GAME;
+        pausePanel.SetActive(false);
     }
 }
